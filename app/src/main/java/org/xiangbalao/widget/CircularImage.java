03fe49b336812578252fce2d0008d23d9de628e6
longtaoge <61852263@qq.com>
2016-10-21 09:18:25 +0800

    修改 自定义控件

--- app/src/main/java/org/xiangbalao/widget/CircularImage.java
+++ app/src/main/java/org/xiangbalao/widget/CircularImage.java
@@ -3,7 +3,6 @@ package org.xiangbalao.widget;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.util.AttributeSet;
