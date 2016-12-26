beff5b7f8c7e361dab4bec6dffb7d727c98d1302
longtaoge <61852263@qq.com>
2016-12-26 18:55:42 +0800

    添加示例

---
+++ hotfixdemo/src/main/java/org/xiangbalao/tinkerdemo/MainActivity.java
@@ -0,0 +1,218 @@
+package org.xiangbalao.tinkerdemo;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Typeface;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.taobao.hotfix.HotFixManager;
+import com.taobao.hotfix.PatchLoadStatusListener;
+import com.taobao.hotfix.demo.R;
+
+import org.xiangbalao.tinkerdemo.app.ApplicationLike;
+
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final int REQUEST_EXTERNAL_STORAGE_PERMISSION = 0;
+
+    private RelativeLayout contentMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+        // 修复BUG
+        //  contentMain.setBackgroundResource(R.mipmap.rooster);
+
+        initHotfix();
+    }
+
+
+    /**
+     * 初始化
+     */
+    private void initView() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        contentMain = (RelativeLayout) findViewById(R.id.content_main);
+
+
+
+
+        final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        final ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawer.openDrawer(GravityCompat.START);
+
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.nav_loadPatch) {
+            //加载补丁
+
+        } else if (id == R.id.nav_loadLibrary) {
+            //加载 SO
+
+        } else if (id == R.id.nav_cleanPatch) {
+            //清除补丁
+
+
+        } else if (id == R.id.nav_killSelf) {
+            //重启应用
+            android.os.Process.killProcess(android.os.Process.myPid());
+            System.exit(0);
+
+        } else if (id == R.id.nav_info) {
+
+
+            openBrowser(getString(R.string.tinker));
+
+
+        } else if (id == R.id.nav_share) {
+            //分享
+            shareMsg(getString(R.string.hotfix), getString(R.string.tinkerdome), getString(R.string.share));
+
+        } else if (id == R.id.nav_about) {
+            openBrowser(getString(R.string.about));
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+    public void shareMsg(String activityTitle, String msgTitle, String msgText) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_SUBJECT, msgTitle);
+        intent.putExtra(Intent.EXTRA_TEXT, msgText);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(Intent.createChooser(intent, activityTitle));
+    }
+
+
+    private void openBrowser(String url) {
+        Uri uri = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+
+    /**
+     * 显示信息
+     *
+     * @param
+     * @return
+     */
+    public boolean showInfo(String string) {
+        // add more Build Info
+
+        final TextView v = new TextView(MainActivity.this);
+        v.setText(string);
+        v.setGravity(Gravity.LEFT | Gravity.CENTER_VERTICAL);
+        v.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 10);
+        v.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        v.setTextColor(0xFF000000);
+        v.setTypeface(Typeface.MONOSPACE);
+        final int padding = 16;
+        v.setPadding(padding, padding, padding, padding);
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setCancelable(true);
+        builder.setView(v);
+        final AlertDialog alert = builder.create();
+        alert.show();
+        return true;
+    }
+
+
+    /**
+     * 为了测试的方便, initialize放在activity中, 但是实际上initialize应该放在Application的onCreate方法中, 需要尽可能早的做初始化.
+     */
+    private void initHotfix() {
+        HotFixManager.getInstance().initialize(this.getApplication(), ApplicationLike.appVersion, ApplicationLike.appId, true, new PatchLoadStatusListener() {
+            @Override
+            public void onload(final int mode, final int code, final String info, final int handlePatchVersion) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        StringBuilder stringBuilder = new StringBuilder();
+                        stringBuilder.append("Mode:").append(mode).append(" Code:").append(code).append(" Info:").append(info).append(" HandlePatchVersion:").append(handlePatchVersion);
+                        showInfo(stringBuilder.toString());
+                    }
+                });
+            }
+        });
+        if (Build.VERSION.SDK_INT >= 23) {
+            requestExternalStoragePermission();
+        }
+    }
+
+
+    /**
+     * 如果本地补丁放在了外部存储卡中, 6.0以上需要申请读外部存储卡权限才能够使用. 应用内部存储则不受影响
+     */
+    private void requestExternalStoragePermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    REQUEST_EXTERNAL_STORAGE_PERMISSION);
+        }
+    }
+
+
+}
---
+++ hotfixdemo/src/main/java/org/xiangbalao/tinkerdemo/app/ApplicationLike.java
@@ -0,0 +1,38 @@
+package org.xiangbalao.tinkerdemo.app;
+
+import android.app.Application;
+
+
+/**
+ * Created by longtaoge on 16/10/21.
+ */
+
+
+
+public class ApplicationLike extends Application {
+
+    public static String appVersion;
+    public static String appId;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initApp();
+
+    }
+
+    private void initApp() {
+        this.appId = ""; //替换掉自己应用的appId  83914-1    69921-1
+        try {
+            this.appVersion = this.getPackageManager().getPackageInfo(this.getPackageName(), 0).versionName;
+        } catch (Exception e) {
+            this.appVersion = "1.0.0";
+        }
+    }
+
+
+
+}
+
+
+
